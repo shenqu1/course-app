7e96e1993211ba08aac11c7d5b42a91c541421f8
Shen Qu <qushen024@hotmail.com>
2021-08-31 17:50:34 -0400

    Styles and Demo page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core'
+import {Routes, RouterModule} from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'demo',
+        loadChildren: () =>
+          import('./pages/demo/demo.module').then((m) => m.DemoModule),
+      },
+    ],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import {AngularFireStorageModule} from '@angular/fire/storage'
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 
-import {environment} from '@src/environments/environment'
+import {environment} from '@src/environments/environment';
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
